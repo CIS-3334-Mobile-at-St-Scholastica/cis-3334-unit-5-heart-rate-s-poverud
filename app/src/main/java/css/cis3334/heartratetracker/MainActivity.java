66452b5404e357f614b6c3ec43b9487004f56ba0
Spoverud <spoverud@css.edu>
2018-02-19 15:54:41 -0600

    Participation Activity

--- app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
+++ app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
@@ -2,17 +2,42 @@ package css.cis3334.heartratetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    HeartRateList heartRateList;        // The list of heart rate objects
+    ListView lvHeartRates;
+    TextView tvSelect;
+    HeartRateList heartRateList;
+    ArrayAdapter<HeartRate> hrAdapter;
+
+    //ArrayList<HeartRate> basicheartRateList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvSelect = (TextView) findViewById(R.id.textViewSelect);
+        lvHeartRates = (ListView) findViewById(R.id.listViewHeartRate);
         heartRateList = new HeartRateList();
         heartRateList.InitRandomElderly();
+
+        hrAdapter = new HeartRateAdapter(this, R.layout.heart_rate_row, R.id.textViewPulse, heartRateList);
+        hrAdapter.setDropDownViewResource(R.layout.heart_rate_row);
+        lvHeartRates.setAdapter(hrAdapter);
+
+        lvHeartRates.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                HeartRate hr = (HeartRate) parent.getItemAtPosition(position);
+                tvSelect.setText("You selected: " + hr.toString());
+            }
+        });
+
     }
 }
